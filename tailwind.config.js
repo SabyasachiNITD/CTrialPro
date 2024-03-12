26b971931816c6cf4ed89d752c937793f18a749f
sabyasachi ghosh <sabyasachi9733@gmail.com>
2024-03-12 23:57:17 +0530

    BG created with autotype

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ module.exports = {
         md:"768px",
         lg:"1024px",
         xl:"1280px",
-      }
+      },
+      spacing: {
+        big: "44rem"
+      },
     },
     fontFamily: {
       nunito: ['Nunito','sans-serif']
